85d6daf57eeb622062daf857400cfc905242ff5d
chen.kim <chen.kim@kakaocorp.com>
2022-12-11 14:07:43 +0900

    [SETTING] root 위치로 파일 이동

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "first post",
+        "excerpt":"Hi there  ","categories": ["category"],
+        "tags": ["first"],
+        "url": "/category/first-post/",
+        "teaser": null
+      }]
